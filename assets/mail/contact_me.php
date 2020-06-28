98c8dc44842344a9ccc5147e4aceb06b611f6d1f
Adin Hudson <adinhudson1996@gmail.com>
2020-06-28 11:20:50 +0530

    not found page

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -16,11 +16,12 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'adinhudson@gmail.com'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'careers@3cs.lk'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
+$headers = "From: adin-agency-application.herokuapp.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";
+$headers .= "This is a test email sent from a website created for 3CS Solutions";
 mail($to,$email_subject,$email_body,$headers);
 return true;         
 ?>
